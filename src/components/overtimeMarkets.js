effc0bcf3223e2131d08328ecbb4cd1a88b97085
Ryan Bharath <dbossman60@gmail.com>
2023-11-13 23:44:33 -0500

    style: styling the carousel item component

--- src/components/overtimeMarkets.js
+++ src/components/overtimeMarkets.js
@@ -6,36 +6,47 @@ import Button from 'react-bootstrap/Button'
 
 function OvertimeMarkets() {
   return (
-    <Carousel data-bs-theme="dark">
-      <Carousel.Item>
+    <Carousel data-bs-theme="dark" style={{ backgroundColor: '#f5f5f5' }}>
+      <Carousel.Item style={{ padding: '1rem', border: '1px solid #ccc' }}>
         <BaseOverTimeMarketsItem />
         <Carousel.Caption>
           <h3>
-            Overtime Markets on <a href="https://base.org">Base</a>
+            <a href="https://base.org">Base</a> Overtime Markets
           </h3>
-          <Button variant="outline-light" href="http://overtimemarkets.xyz">
+          <Button
+            style={{ color: 'black' }}
+            variant="outline-light"
+            size="lg"
+            href="http://overtimemarkets.xyz"
+          >
             Play Now
           </Button>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item style={{ padding: '1rem', border: '1px solid #ccc' }}>
         <OptimismOverTimeMarketsItem />
         <Carousel.Caption>
           <h3>
-            Overtime Markets on <a href="http://optimism.io/">Optimism</a>
+            <a href="http://optimism.io/">Optimism</a> Overtime Markets
           </h3>
-          <Button variant="outline-light" href="http://overtimemarkets.xyz">
+          <Button
+            style={{ color: 'black' }}
+            variant="outline-light"
+            size="lg"
+            href="http://overtimemarkets.xyz"
+          >
             Play Now
           </Button>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item style={{ padding: '1rem', border: '1px solid #ccc' }}>
         <ArbitriumOverTimeMarketsItem />
         <Carousel.Caption>
           <h3>
-            Overtime Markets on <a href="http://arbitrum.io">Arbitrium</a>
+            <a href="http://arbitrum.io">Arbitrium</a> Overtime Markets
           </h3>
           <Button
+            style={{ color: 'black' }}
             variant="outline-light"
             size="lg"
             href="http://overtimemarkets.xyz"
