2d9212c6a54fb9a124807d509e2a5e69b4421f24
Ryan Bharath <dbossman60@gmail.com>
2023-11-15 23:56:14 -0500

    fix: random generated number only goes from 0 to the length of the array - 1

--- src/components/scripts/GenerateRandomIndex.js
+++ src/components/scripts/GenerateRandomIndex.js
@@ -1,7 +1,7 @@
 function GenerateRandomIndex(marketsArray) {
   var dataLength = marketsArray.length
-  var min = Math.ceil(0)
-  var max = Math.floor(dataLength)
+  var min = 0
+  var max = dataLength - 1
   const randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
   return randomNumber
 }
