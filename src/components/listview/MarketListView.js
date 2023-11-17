bf8bc5a78c44340ed2504a059f39fd81bd677045
Ryan Bharath <dbossman60@gmail.com>
2023-11-17 18:42:58 -0500

    chore: basic listview with team names

--- src/components/listview/MarketListView.js
+++ src/components/listview/MarketListView.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Tab, Tabs } from 'react-bootstrap'
+import { chainIds } from '../scripts/SupportedChains'
+import ErrorMessage from '../ui/ErrorMessage'
+import Loader from '../ui/Loader'
 
-function MarketListView() {
-  return <div>MarketListView</div>
+const MarketListView = () => {
+  const [activeTab, setActiveTab] = useState('base')
+  const [markets, setMarkets] = useState([])
+  const [chain, setActiveChain] = useState(8453)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const url = `https://api.thalesmarket.io/overtime/networks/${chain}/markets?ungroup=true`
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setMarkets(data)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setIsLoading(false)
+      })
+  }, [url])
+
+  if (isLoading) return Loader()
+  if (error) return ErrorMessage()
+
+  if (!markets) {
+    return <p>Error: Data could not be fetched.</p>
+  }
+
+  return (
+    <Tabs
+      id="market-tabs"
+      activeKey={activeTab}
+      onSelect={(tab) => setActiveTab(tab)}
+      className="mb-3"
+    >
+      <Tab eventKey="base" title="Base">
+        {markets && (
+          <div className="market-list">
+            {markets.map((market) => (
+              <div key={market.gameId}>
+                <h2>{market.homeTeam}</h2>
+                <p>{market.awayTeam}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </Tab>
+      <Tab eventKey="arbitrum" title="Arbitrium">
+        {markets && (
+          <div className="market-list">
+            {markets.map((market) => (
+              <div key={market.gameId}>
+                <h2>{market.homeTeam}</h2>
+                <p>{market.awayTeam}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </Tab>
+      <Tab eventKey="optimism" title="Optimism">
+        {markets && (
+          <div className="market-list">
+            {markets.map((market) => (
+              <div key={market.gameId}>
+                <h2>{market.homeTeam}</h2>
+                <p>{market.awayTeam}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </Tab>
+    </Tabs>
+  )
 }
 
 export default MarketListView
---
+++ src/components/scripts/SupportedChains.js
@@ -0,0 +1,5 @@
+export const chainIds = {
+    base: 8453,
+    arbitrum: 42161, 
+    optimism: 10,
+}
