56154c5826069806926a0c01aa915ba8296f9101
Ryan Bharath <dbossman60@gmail.com>
2023-11-18 00:43:49 -0500

    style: header along with making it responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Nav, NavItem } from 'react-bootstrap'
+import { Nav, NavItem, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <Nav variant="tabs" defaultActiveKey="1">
-      <NavItem>
-        <Link to="/" className="nav-link">
-          Featured Markets
-        </Link>
-      </NavItem>
-      <NavItem>
-        <Link to="/markets" className="nav-link">
-          Market List
-        </Link>
-      </NavItem>
-    </Nav>
+    <Navbar bg="light" expand="lg" className="mb-3">
+      <Navbar.Brand href="/">Rollup Markets</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavItem>
+            <Link to="/" className="nav-link">
+              Featured Markets
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/markets" className="nav-link">
+              Market List
+            </Link>
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
